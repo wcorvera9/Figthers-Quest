30220c3bc68c37331003706d5e42036224ae0871
wcorvera9 <00128526@uca.edu.sv>
2026-06-15 17:25:36 -0600

    ModocombateEdit2

--- CombateV1.cpp
+++ CombateV1.cpp
@@ -2,4 +2,17 @@
 #include "PersonajesV2.cpp" 
 using namespace std;
 
-
+// revisa que todavia este alguien vivo del cualquiera del equipo 
+bool estavivo(personajes equipo[], int tam){
+    for (int i = 0; i < tam; i++)
+    {
+        if (equipo[i].estavivo()){ // si encuentra al menos uno vivo es true y no sigue revisando
+            return true;
+        }
+        return false; // si sale del for todos están muertos
+    }
+    
+}
+int cambiarpersonaje(personajes equipo[], int tam, int usando, int jugador){
+    
+}
