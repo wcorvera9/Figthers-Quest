911dec3a9871fc331c646f3c5486e0db332f50f3
wcorvera9 <00128526@uca.edu.sv>
2026-06-26 10:47:31 -0600

    Combateedit4

--- CombateV1.cpp
+++ CombateV1.cpp
@@ -4,11 +4,14 @@
 using namespace std;
 
 void elegirEquipo(Personaje equipo[]) {
-    cout << "Elige 3 personajes:" << endl;
+    cout << "Elige 3 personajes:\n";
 
+    // Mostrar personajes disponibles
     for (int i = 0; i < TOTAL_PERSONAJES; i++) {
         cout << i << ") " << listaPersonajes[i].nombre << endl;
     }
+
+    // Elegir 3
     for (int i = 0; i < 3; i++) {
         int op;
         cout << "Personaje " << i + 1 << ": ";
@@ -16,10 +19,28 @@ void elegirEquipo(Personaje equipo[]) {
         equipo[i] = listaPersonajes[op];
     }
 }
- bool equipoVivo(Personaje equipo[]) {
+
+bool equipoVivo(Personaje equipo[]) {
     for (int i = 0; i < 3; i++) {
-    if (equipo[i].estaVivo())
-    return true;
+        if (equipo[i].estaVivo())
+            return true;
     }
-return false;
+    return false;
 }
+
+void combate() {
+    Personaje equipo[3];
+    Personaje jefe("JEFE", 300, 30, 0);
+    int activo = 0;
+
+    elegirEquipo(equipo);
+
+    while (equipoVivo(equipo) && jefe.estaVivo()) {
+
+        
+        if (!equipo[activo].estaVivo()) {
+            activo++;
+            continue;
+        }
+    }
+
